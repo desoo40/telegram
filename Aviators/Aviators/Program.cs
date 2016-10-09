fd7c8686db2ac17ad640c5f866d595999c03208b
DeSoo <deso40@gmail.com>
2016-10-09 23:33:07 +0300

    token added

--- Aviators/Aviators/Program.cs
+++ Aviators/Aviators/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace Aviators
 {
     class Program
     {
+        static readonly TelegramBotClient Bot = new TelegramBotClient("297610365:AAG3yzYtC0XgLrQC0ong1qdJ5odMkqiGHno");
+
         static void Main(string[] args)
         {
         }
