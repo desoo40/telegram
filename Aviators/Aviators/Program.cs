e58692e27feb0a315e2ba160a9574bc53a4f244f
Dmitry Latokhin <dimonstrik88@gmail.com>
2016-10-10 00:48:08 +0300

    Player added

--- Aviators/Aviators/Program.cs
+++ Aviators/Aviators/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 
@@ -10,9 +12,66 @@ namespace Aviators
     class Program
     {
         static readonly TelegramBotClient Bot = new TelegramBotClient("297610365:AAG3yzYtC0XgLrQC0ong1qdJ5odMkqiGHno");
+        static readonly List<Player> Players = new List<Player>();
+        static readonly FuckGen Fuck = new FuckGen();
+        static bool End = true;
 
         static void Main(string[] args)
         {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            LoadPlayers("");
+
+            StartBot();
+        }
+
+        static void LoadPlayers(string path)
+        {
+            Console.WriteLine("Try parse " + path);
+            using (var sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var fields = line.Split(';');
+                    if (fields.Length != 3) continue;
+
+                    Players.Add(new Player(int.Parse(fields[0]),fields[1],fields[2]));
+                }
+            }
+        }
+
+        static void StartBot()
+        {
+            var me = Bot.GetMeAsync().Result;
+            Console.WriteLine("Hello my name is " + me.FirstName);
+            Console.WriteLine("Press ctrl+c to kill me.");
+
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.StartReceiving();
+
+            while (End)
+            {
+                //Nothing to do, just sleep 1 sec
+                //ctrl+c break cycle
+                Thread.Sleep(1000);
+            }
+
+            Bot.StopReceiving();
+        }
+
+        private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+
+            await Bot.SendTextMessage(e.Message.Chat.Id, Fuck.GetFuck());
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("It's the end! Bye.");
+            End = false;
+            e.Cancel = true;
         }
     }
 }
