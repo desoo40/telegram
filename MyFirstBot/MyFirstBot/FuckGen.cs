5b766fb268f9005c4a4c4751944e6743870c00fd
Dmitry Latokhin <dimonstrik88@gmail.com>
2016-10-09 03:04:59 +0300

    Simple bot reaction added

---
+++ MyFirstBot/MyFirstBot/FuckGen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBot
+{
+    public class FuckGen
+    {
+        private readonly List<string> fucks = new List<string>();
+        private readonly Random random = new Random();
+
+        public FuckGen()
+        {
+            InitializateFucks();
+        }
+
+        public string GetFuck()
+        {
+            var index = random.Next(fucks.Count);
+            return fucks[index];
+        }
+
+        private void InitializateFucks()
+        {
+            fucks.Add("Пошёл нахуй!");
+            fucks.Add("Соси");
+            fucks.Add("Fuck you!");
+            fucks.Add("Ты залупа!");
+            fucks.Add("А может ты залупоед?");
+            fucks.Add("Знаешь что я делал с твоей мамкой?");
+            fucks.Add("Подрочи, полегчает.");
+        }
+    }
+}
