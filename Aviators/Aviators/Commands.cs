e0bf9a2aa9399247f96fce90c9e128b35a58ee24
DeSoo <deso40@gmail.com>
2016-10-13 00:28:01 +0300

    сделан каркас методов команд FuckGen переименован в Randomiser добавлен файл расписания парсинг команд полностью перенесен в класс Commands добавлены команды /помощь и /кричалки

--- Aviators/Aviators/Commands.cs
+++ Aviators/Aviators/Commands.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Telegram.Bot;
 
@@ -11,7 +13,7 @@ namespace Aviators
     public class Commands
     {
         private TelegramBotClient Bot;
-        readonly FuckGen Fuck;
+        readonly Randomiser Gen;
         DBCore DB;
 
         string DBPlayersPhotoDirPath = Directory.GetCurrentDirectory() + @"/data_base/PlayersPhoto/";
@@ -19,7 +21,7 @@ namespace Aviators
         public Commands(TelegramBotClient bot)
         {
             Bot = bot;
-            Fuck = new FuckGen();
+            Gen = new Randomiser();
             DB = new DBCore();
         }
 
@@ -47,15 +49,14 @@ namespace Aviators
                 }
                 else
                 {
-                    var playerDescription = Fuck.GetFuck();
+                    var playerDescription = Gen.GetPlayerDescr();
                     playerDescription += string.Format("#{0} {1} {2}", player.Number, player.Name,
                         player.Surname);
                     var photo = new Telegram.Bot.Types.FileToSend(player.Number + ".jpg",
                         (new StreamReader(Path.Combine(DBPlayersPhotoDirPath, player.PhotoFile))).BaseStream);
 
                     Console.WriteLine($"Send player:{player.Surname}");
-                    ;
-                ;
+                    
                     await Bot.SendPhotoAsync(chatFinded.Id, photo, playerDescription);
 
                 }
@@ -66,6 +67,94 @@ namespace Aviators
                 await Bot.SendTextMessageAsync(chatFinded.Id, "Больше так не делай, мне больно(");
 
             }
+        }
+
+        public async void FindCommand(string msg, Chat chatFinded)
+        {
+            Regex rxNums = new Regex(@"^\d+$"); // делаем проверку на число
+            if (rxNums.IsMatch(msg))
+            {
+                ShowPlayerByNubmer(msg, chatFinded);
+            }
+            else
+            {
+                msg = msg.ToLower();
+                var fields = msg.Split(' ');
+
+                if (fields[0] == "помощь")
+                {
+                    Help(chatFinded);
+                    return;
+                }
+                if (fields[0] == "статистика")
+                {
+                    Statistic(chatFinded, fields);
+                    return;
+                }
+                if (fields[0] == "расписание")
+                {
+                    TimeTable(chatFinded, fields);
+                    return;
+                }
+                if (fields[0] == "следующая")
+                {
+                    NextGame(chatFinded);
+                    return;
+                }
+                if (fields[0] == "соперник")
+                {
+                    EnemyTeam(chatFinded, fields);
+                    return;
+                }
+                if (fields[0] == "кричалки")
+                {
+                    Slogans(chatFinded);
+                    return;
+                }
+
+                await Bot.SendTextMessageAsync(chatFinded.Id, "Неверная команда, воспользуйтесь /помощь");
+            }
+        }
+
+        private async void Slogans(Chat chatFinded)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id, Gen.GetSlogan());
+        }
+
+        private async void EnemyTeam(Chat chatFinded, string[] fields)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id, "Привет, я соперник");
+        }
+
+        private async void NextGame(Chat chatFinded)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id, "Привет, я следующая игра");
+        }
+
+        private async void TimeTable(Chat chatFinded, string[] fields)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id, "Привет, я расписание");
+        }
+
+        private async void Statistic(Chat chatFinded, string[] fields)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id, "Привет, я статистика");
+        }
+
+        public async void Help(Chat chatFinded)
+        {
+            await Bot.SendTextMessageAsync(chatFinded.Id,
+                "Мною можно управлять с помощью команд:\n" +
+                "(можно вводить без '/') \n" +
+                "\n" +
+                "'номер' - поиск игрока по номеру\n\n" +
+                "/статистика 'номер' - статистика игрока\n\n" +
+                "/статистика 'фамилия' - можно также через фамилию\n\n" +
+                "/расписание 'n' - расписание ближайших n игр\n\n" +
+                "/следующая - дата, время, соперник и место следующей игры\n\n" +
+                "/соперник 'название команды' - история встреч\n\n" +
+                "/кричалки - выводит одну из кричалок команды\n\n" +
+                "/помощь - помощь по управлению\n\n");
         }
     }
 }
