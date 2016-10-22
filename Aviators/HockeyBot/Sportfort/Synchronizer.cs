36a3ce3789dc604b85b9311fed810ea963e6c27e
Dmitry Latokhin <dimonstrik88@gmail.com>
2016-10-22 06:56:32 +0300

    fix devenv git add bug

---
+++ Aviators/HockeyBot/Sportfort/Synchronizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HockeyBot.Configs;
+
+namespace HockeyBot.Sportfort
+{
+    public class Synchronizer
+    {
+        public static void InitializateSources()
+        {
+            var players = TeamMembersPageParser.ParseTeamMembersPage(new Uri(Config.SportFortTeamMembersPage));
+            foreach(var player in players)
+            {
+                Console.WriteLine(player.Name);
+            }
+
+        }
+        public static void UpdateSources()
+        {
+
+        }
+    }
+}
