c072e26b2076b4175f6a22b7e11a76e3b24dab19
desoo40 <deso40@gmail.com>
2017-03-19 23:04:50 +0300

    parseTXT work start

---
+++ Aviators/Aviators/Data/ParseTXT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aviators
+{
+    class ParseTXT
+    {
+        List<Player> Roster = new List<Player>();
+    }
+}
