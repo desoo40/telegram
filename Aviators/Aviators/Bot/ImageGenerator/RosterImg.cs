e34e11ca279fb0ad267da65c649db92aa23a239c
desoo40 <deso40@gmail.com>
2017-06-18 23:49:02 +0300

    создан класс для разметки состава

---
+++ Aviators/Aviators/Bot/ImageGenerator/RosterImg.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Aviators.Bot.ImageGenerator
+{
+    public class RosterImg
+    {
+        public Rectangle EnemyLogo { get; set; }
+        public Rectangle TournamentLogo { get; set; }
+
+        public class Player
+        {
+            public Photo Photo { get; set; }
+            public TextInImg Name { get; set; }
+            public TextInImg Number { get; set; }
+            public TextInImg AorK { get; set; }
+
+
+
+        }
+
+        public class Photo
+        {
+            public Rectangle Position { get; set; }
+            public int OffsetX { get; set; }
+            public int OffsetY { get; set; }
+        }
+    }
+}
